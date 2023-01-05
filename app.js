15447d92a2779593266f123629d4ba0b39f84a7c
sha-zha <adeline.servan@gmail.com>
2023-01-05 12:12:25 +0100

    connection à la base de donnée

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+/***************Mongodb configuratrion********************/
+const mongoose = require('mongoose');
+const configDB = require('./config/database.js');
 
-var app = express();
+mongoose.set('strictQuery', false);
+mongoose.connect(configDB.url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+ 
+})
+  .then(() => {
+    console.log('connection to database established successfully');
+  })
+  .catch(err => {
+    console.log('An error occursed ', err);
+  });
+
+/***********************************/
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
